4de2b52226aca333de97519cac4165fced1f535a
梁润飞 <zc@runffy.com>
2026-05-25 16:48:46 +0800

    remove ie related, v20260525-1648

--- F2B.Browser.IExplore/F2B.Browser.IExplore/IeBaseOn.cs
+++
@@ -1,9 +0,0 @@
-namespace F2B.Browser.IExplore
-{
-    /// <summary>Whether an activity operates on the window document or an element context.</summary>
-    public enum IeBaseOn
-    {
-        Window,
-        Element
-    }
-}
--- F2B.Browser.IExplore/F2B.Browser.IExplore/OperationDefaults.cs
+++
@@ -1,8 +0,0 @@
-namespace F2B.Browser.IExplore
-{
-    public static class OperationDefaults
-    {
-        /// <summary>Default timeout (ms) for Connect, Click, and Input.</summary>
-        public const int TimeoutMs = 15000;
-    }
-}
